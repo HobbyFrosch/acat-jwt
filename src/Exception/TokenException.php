aa0cf512e0b3de6b2bed2f5b62197f4805190106
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2025-07-15 20:35:13 +0200

    sds

--- src/Exception/TokenException.php
+++ src/Exception/TokenException.php
@@ -10,11 +10,11 @@ use Throwable;
 class TokenException extends \Exception {
 
     /**
-     * @param string $message
-     * @param int $code
-     * @param Throwable|null $previous
+     * @param   string        $message
+     * @param   int           $code
+     * @param Throwable|null  $previous
      */
-    public function __construct($message = "", $code = 0, Throwable $previous = null) {
+    public function __construct(string $message = "", int $code = 401, ?Throwable $previous = null) {
         parent::__construct($message, $code, $previous);
     }
 
